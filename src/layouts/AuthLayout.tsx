496d28f1742d44d5b53a50ea6763224cdd716a8e
Luis Guillermo Pérez Rodríguez <luisguillermo.rodriguez32@gmail.com>
2025-03-25 16:24:21 -0400

    style: adjust max height of AuthLayout container for better usability

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -3,7 +3,7 @@ import { Link, Outlet } from "react-router";
 const AuthLayout = () => {
   return (
     <div className="flex h-screen flex-1 flex-col justify-center p-8">
-      <div className="bg-white max-h-3/4 overflow-y-scroll p-8 shadow w-full self-center rounded-xl sm:w-lg">
+      <div className="bg-white max-h-11/12 overflow-y-scroll p-8 shadow w-full self-center rounded-xl sm:w-lg">
         <Link to="/">
           <img
             alt="Chatrium"
