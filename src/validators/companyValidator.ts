801237ef701fc1ca8a6a6461fc6cbd1361c0e186
Luis Guillermo Pérez Rodríguez <luisguillermo.rodriguez32@gmail.com>
2025-04-10 09:36:55 -0400

    fix errors

---
+++ src/validators/companyValidator.ts
@@ -0,0 +1,23 @@
+import { z } from "zod";
+
+const companyValidator = z
+  .string()
+  .max(20)
+  .refine((value) => !/^\s/.test(value), {
+    message: "Company name cannot start with a space",
+  })
+  .refine((value) => /[a-zA-Z]/.test(value), {
+    message: "Company name must contain at least one letter",
+  })
+  .refine((value) => (value.match(/&/g) || []).length <= 5, {
+    message: "Company name can contain up to 5 ampersands (&)",
+  })
+  .refine((value) => (value.match(/[-_]/g) || []).length <= 4, {
+    message: "Company name can contain up to 4 hyphens or underscores",
+  })
+  .refine((value) => /^[a-zA-Z0-9 &_-]*$/.test(value), {
+    message:
+      "Only letters, numbers, spaces, ampersands, hyphens, and underscores are allowed",
+  });
+
+export default companyValidator;
