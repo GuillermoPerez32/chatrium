4d167cc74d5dd3d37555b315c683606bc52d0818
Luis Guillermo Pérez Rodríguez <luisguillermo.rodriguez32@gmail.com>
2025-04-10 22:21:46 -0400

    fix theme

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,23 +1,19 @@
 import { Button } from "./ui/button";
-import { Computer, Moon, Sun } from "lucide-react";
+import { Moon, Sun } from "lucide-react";
 import { useTheme } from "@/hooks";
 
 const ThemeToggle = () => {
   const { theme, setTheme } = useTheme();
 
   const toggleTheme = () => {
-    setTheme(
-      theme === "light" ? "dark" : theme === "dark" ? "system" : "light"
-    );
+    setTheme(theme === "light" ? "dark" : "light");
   };
   return (
     <Button variant="ghost" onClick={toggleTheme} className="relative">
       {theme === "dark" ? (
         <Sun className="w-6 h-6 text-muted-foreground" />
-      ) : theme === "light" ? (
-        <Moon className="w-6 h-6 text-muted-foreground" />
       ) : (
-        <Computer className="w-6 h-6 text-muted-foreground" />
+        <Moon className="w-6 h-6 text-muted-foreground" />
       )}
     </Button>
   );
